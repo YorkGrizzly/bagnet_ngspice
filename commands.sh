e31d64c940cd335b18ba290fce2b7964255f505a
YorkGrizzly <yorkygrizzly@gmail.com>
2022-01-04 19:22:16 -0800

    update commands.sh comments

--- commands.sh
+++ commands.sh
@@ -5,7 +5,7 @@
 ### two stage oracle algorithm (takes long)
 #./run.sh deep_ckt/efficient_ga/run_scripts/main.py specs/iccad/two_stage_oracle_custom_ea.yaml
 
-### two stage bagnet algorithm
+### two stage bagnet algorithm   (need to change 'n_query' to 'n_nn_query' in deep_ckt/efficient_ga/src/bagnet/viz/plot.py line 107)
 ./run.sh deep_ckt/efficient_ga/run_scripts/main.py specs/iccad/two_stage_bagnet_custom_ea_dropout_multi_sampling.yaml
 
 ### to plot figures on top of eachother use the following script
